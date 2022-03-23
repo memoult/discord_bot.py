caa10b7a91e230476cc2c733de76924c1bd93286
Legion13 <59478217+memoult@users.noreply.github.com>
2022-03-24 02:55:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord.ext.commands import bot
 import os
 
 bot = commands.Bot(command_prefix = "!", intents = discord.Intents.all())
-token = 'ODcyOTI1MjQzNjM3OTE1Njg5.YQw8_A.oiTZbafv-hPwoMqdENugqKHY6qk'
+token = 'token'
 
 @bot.command()
 @commands.is_owner()
@@ -30,4 +30,4 @@ for filename in os.listdir("./cogs"):
 	if filename.endswith(".py"):
 		bot.load_extension(f"cogs.{filename[:-3]}")
 
-bot.run('ODcyOTI1MjQzNjM3OTE1Njg5.YQw8_A.oiTZbafv-hPwoMqdENugqKHY6qk')
+bot.run('token')
